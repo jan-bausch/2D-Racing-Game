ed6760d8ecbb3f14b77d8df4eccaa0f8d87c06f9
Jan F. Bausch <jfmulti@gmail.com>
2015-07-03 17:25:58 +0200

    ADD: Better movement-simulation

--- src/app/systems/InputSystem.hx
+++ src/app/systems/InputSystem.hx
@@ -16,11 +16,15 @@ class InputSystem extends System {
 	private var inputNodes: NodeList<InputNode>;
 	private var scene: Sprite;	//Verweis auf die Applikation, um Zugriff auf Keyboard-Events zu erhalten.
 
-    private var maxSteerAngle: Float;   //Maximaler Auschlag der Räder
-    private var steerAmount: Float;     //Empfindlichkeit des Lenkrads
-    private var maxSpeed: Float;    //Maximale Geschwindigkeit des Autos
-    private var acceleration: Float;    //Beschleunigung
-    private var deceleration: Float;
+    //Physikalische Konstanten
+    private var MAX_STEER_ANGLE: Float;   //Maximaler Auschlag der Räder
+    private var STEER_SPEED: Float;     //Empfindlichkeit des Lenkrads
+    private var MAX_SPEED: Float;    //Maximale Geschwindigkeit des Autos
+    private var ACCELERATION: Float;    //Beschleunigung
+    private var DECELERATION: Float;    //Entschleunigung
+    private var BREAK: Float;    //Bremskraft
+
+    //Steurungsinput
     private var up: Bool;
     private var down: Bool;
     private var right: Bool;
@@ -36,11 +40,14 @@ class InputSystem extends System {
         left = false;
 
         //Physikalische Konstanten festlegen
-        maxSteerAngle = 45;  //45°
-        steerAmount = 200;                     
-        maxSpeed = 200;
-        acceleration = 30;
-        deceleration = 15;
+        MAX_STEER_ANGLE = 45;
+
+        STEER_SPEED = 120;
+        MAX_SPEED = 800;
+        ACCELERATION = 100;
+        DECELERATION = 130;
+        BREAK = 200;
+
 
         //Events registrieren
         scene.addEventListener(Event.ADDED_TO_STAGE, onAddedToStage);
@@ -54,16 +61,33 @@ class InputSystem extends System {
             var vehicle: Vehicle = inputNode.vehicle;
 
 
-            //Lenkrad drehen, sofern nicht über maximalem Ausschlag.
-            if (right && vehicle.steerAngle + steerAmount * elapsed <= maxSteerAngle) vehicle.steerAngle += steerAmount * elapsed;
-            if (left && vehicle.steerAngle - steerAmount * elapsed >= -maxSteerAngle) vehicle.steerAngle -= steerAmount * elapsed;
+            //Lenkrad wieder in Nullstellung bringen
+            var STRAIGHTENING_SPEED = STEER_SPEED * elapsed * vehicle.speed / 1000;
 
-            if (up && maxSpeed >= vehicle.speed + acceleration * elapsed) {
-                vehicle.speed += acceleration * elapsed;
-            } else {
-                vehicle.speed -= deceleration * elapsed;
-                if (vehicle.speed < 0) vehicle.speed = 0;
+            if (vehicle.steerAngle > 0) {
+                vehicle.steerAngle = (vehicle.steerAngle - STRAIGHTENING_SPEED) > 0 ? vehicle.steerAngle - STRAIGHTENING_SPEED : 0;
             }
+
+            if (vehicle.steerAngle < 0) {
+                vehicle.steerAngle = (vehicle.steerAngle + STRAIGHTENING_SPEED) < 0 ? vehicle.steerAngle + STRAIGHTENING_SPEED : 0;
+            }   
+
+
+
+            //Lenkrad drehen, sofern nicht über maximalem Ausschlag.
+            if (right && vehicle.steerAngle + STEER_SPEED * elapsed <= MAX_STEER_ANGLE) vehicle.steerAngle += STEER_SPEED * elapsed;
+            if (left && vehicle.steerAngle - STEER_SPEED * elapsed >= -MAX_STEER_ANGLE) vehicle.steerAngle -= STEER_SPEED * elapsed;
+
+            //Wenn geradeaus gefahren wird und maximale Geschwindigkeit noch nicht erreich ist, wird das Auto beschleunigt.
+            if (up && MAX_SPEED >= vehicle.speed + ACCELERATION * elapsed) {
+                vehicle.speed += ACCELERATION * elapsed;
+            } else {
+                vehicle.speed -= DECELERATION * elapsed;
+            }
+
+            //Abbremsen
+            if (down) vehicle.speed -= BREAK * elapsed;
+            if (vehicle.speed < 0) vehicle.speed = 0;
 
 
         }   
