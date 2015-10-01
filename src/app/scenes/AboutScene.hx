4c36173781b78a20ec4f04a5d649ef0cfd93e8af
Jan F. Bausch <jfmulti@gmail.com>
2015-10-01 17:36:27 +0200

    Added reset-button for deleting configuration

--- src/app/scenes/AboutScene.hx
+++ src/app/scenes/AboutScene.hx
@@ -4,6 +4,8 @@ import haxe.ui.toolkit.core.interfaces.IDisplayObjectContainer;
 import haxe.ui.toolkit.core.PopupManager;
 import haxe.ui.toolkit.controls.CheckBox;
 import haxe.ui.toolkit.controls.OptionBox;
+import haxe.ui.toolkit.controls.Button;
+import haxe.ui.toolkit.events.UIEvent;
 import haxe.ui.toolkit.core.Toolkit;
 
 import app.scenes.PopupScene;
@@ -19,11 +21,15 @@ class AboutScene extends PopupScene {
 		width = 500;
 		buttons = [PopupButton.OK];
 
-
 		//Layout laden
 		view = Toolkit.processXmlResource("assets/ui/layout/about.xml");
 
-
+		
+		//Wenn "Zurücksetzen"-Button gedrückt wird, Daten löschen.
+		view.findChild("reset", Button).onClick = function (e:UIEvent) {
+			new Configuration().clear();
+		}
+		
 	}
 
 }
